12a322825e9430c088f7212506a2283ed9fc49d8
Grégory Marini <greg.marini@hotmail.fr>
2022-09-17 11:45:36 +0200

    intégration search avec un délai minimum pour chercher un film via l'api ODMb

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "be1446fd",
+            s: searchTerm,
+        },
+    });
+    console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+let timeoutId;
+
+const onInput = (e) => {
+    timeoutId && clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+        fetchData(e.target.value);
+    }, 1500);
+};
+
+input.addEventListener("input", onInput);
