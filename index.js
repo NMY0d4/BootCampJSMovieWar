f974459d39f1828609876890111be0384f73a102
Grégory Marini <greg.marini@hotmail.fr>
2022-09-17 12:06:33 +0200

    mise en place d'un fonction 'debounce' sur la recherche pour éviter les demande intempestive

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector("input");
 
-let timeoutId;
+const debounce = (func, delayInSeconds = 1) => {
+    let timeoutId;
+    return (...args) => {
+        timeoutId && clearTimeout(timeoutId);
 
-const onInput = (e) => {
-    timeoutId && clearTimeout(timeoutId);
-
-    timeoutId = setTimeout(() => {
-        fetchData(e.target.value);
-    }, 1500);
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delayInSeconds * 1000);
+    };
 };
+
+const onInput = debounce((e) => {
+    fetchData(e.target.value);
+}, 1.5);
 
 input.addEventListener("input", onInput);
